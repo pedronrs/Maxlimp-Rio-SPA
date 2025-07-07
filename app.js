b12f8d1030705352f01ba7b3f6732a05ca37312f
pedro! <pedro.reis.ismart@gmail.com>
2025-07-07 12:31:47 -0300

    html e css pre estruturados

---
+++ app.js
@@ -0,0 +1,27 @@
+            // <div class="section-content home-content">
+            //     <h1 class="section-title">Bem vindo à Maxlimp Rio</h1>
+            //     <p class="section-description">Sua solução em produtos de Limpeza e Higiene.</p>
+            //     <a href="#s-contact" class="btn btn-primary">Fale Conosco</a>
+
+            //  <div class="section-content about-content">
+            //     <h2 class="section-title">Sobre a Maxlimp Rio</h2>
+            //     <div class="about-text-block">
+            //         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit iusto nemo rerum deserunt. Dignissimos vero atque est, quos inventore molestias aspernatur ad asperiores incidunt maxime fugit odio sint voluptatem veritatis!</p>
+            //     </div>
+            // </div>
+
+            //  <div class="section-content contact-content">
+            //     <h2 class="section-title">Entre em Contato</h2>
+            //     <p class="contact-description">Fale conosco para dúvidas e orçamentos.</p>
+            //     <a href="https://wa.me/5521998355672" target="_blank" rel="noopener noreferrer" class="btn btn-whatsapp">
+            //         <img src="assets/whatsapp-icon.svg" alt="Ícone WhatsApp" class="whatsapp-icon">
+            //     </a>
+            //     <div class="contact-info">
+            //         <p><strong>Telefone:</strong>21 98765-4321</p>
+            //         <p><strong>E-mail:</strong>contato.maxlimp@gmail.com</p>
+            //     </div>
+            // </div>
+
+
+        //     <p class="footer-copy">&copy; 2025 Maxlimp Rio. Todos os direitos reservados</p>
+        // <small class="footer-credits">Desenvolvido por: <a href="https://github.com/pedronrs" target="_blank">@pedronrs</a></small>
