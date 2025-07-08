68fc76d8d66dce73d8179822088e010c2bc9a7c7
pedro! <pedro.reis.ismart@gmail.com>
2025-07-08 07:15:04 -0300

    começando a incluir o conteúdo via JS

--- app.js
+++ app.js
@@ -2,6 +2,59 @@
             //     <h1 class="section-title">Bem vindo à Maxlimp Rio</h1>
             //     <p class="section-description">Sua solução em produtos de Limpeza e Higiene.</p>
             //     <a href="#s-contact" class="btn btn-primary">Fale Conosco</a>
+    const homeContent = {
+        title: "Bem-vindo à Maxlimp Rio",
+        description: "Sua solução em produtos de Limpeza e Higiene.",
+        buttonText: "FALE COM UM VENDEDOR",
+        buttonLink: "#s-contact" 
+    }
+
+    function renderHomeSection() {
+        const homeSectionElement = $("#s-home");
+
+        const textButtonBlock = $("<div>");
+        textButtonBlock.addClass("s-home-text-button-block");
+
+        const titleElement = $("<h1>");
+        titleElement.text(homeContent.title);
+
+        const descriptionElement = $('<p>');
+        descriptionElement.text(homeContent.description);
+
+        const btnElement = $("<a>");
+        btnElement.attr('href', homeContent.buttonLink);
+        btnElement.text(homeContent.buttonText);
+        btnElement.addClass('btn btn-cta');
+
+        textButtonBlock.append(titleElement);
+        textButtonBlock.append(descriptionElement);
+        textButtonBlock.append(btnElement);
+
+        homeSectionElement.append(textButtonBlock);
+    }
+    $(document).ready(function(){
+        renderHomeSection();
+    })
+
+    
+    const aboutContent = {
+         title: "Sobre nós",
+         description: "Na Maxlimp Rio, somos especialistas em soluções completas de limpeza profissional e descartáveis para empresas no Rio de Janeiro.Nossa missão é simplificar a gestão de suprimentos, garantindo qualidade, economia e agilidade. Oferecemos uma ampla variedade de produtos de alta performance, com preços especiais para compras em volume. Asseguramos entregas rápidas em até 48 horas, frete grátis e faturamento via boleto bancário, garantindo total competitividade. Ao escolher a Maxlimp Rio, você obtém economia real, qualidade comprovada e suporte personalizado para seu negócio. Estamos prontos para atender suas demandas no atacado e varejo, e será um prazer apresentar uma proposta sob medida para sua próxima cotação. Conte conosco para um ambiente sempre limpo, seguro e produtivo."
+    }
+
+    function renderAbout(){
+
+    }
+
+
+    const contactContent = {
+        title: "Contato",
+
+    }
+
+    function renderContact(){
+
+    }
 
             //  <div class="section-content about-content">
             //     <h2 class="section-title">Sobre a Maxlimp Rio</h2>
@@ -25,3 +78,4 @@
 
         //     <p class="footer-copy">&copy; 2025 Maxlimp Rio. Todos os direitos reservados</p>
         // <small class="footer-credits">Desenvolvido por: <a href="https://github.com/pedronrs" target="_blank">@pedronrs</a></small>
+
